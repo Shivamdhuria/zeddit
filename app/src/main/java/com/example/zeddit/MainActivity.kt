3d7be959938bffcd4ef3cd9b8b3330bc6124e559
Shivam Dhuria <sdhuria@ask-fast.com>
2019-12-17 13:40:54 +0530

    Fixed bottom navigation background color

--- app/src/main/java/com/example/zeddit/MainActivity.kt
+++ app/src/main/java/com/example/zeddit/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.zeddit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coordinator_layout.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
 }
